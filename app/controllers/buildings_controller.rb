ce333ca952ebda3a57e960c106f202709f09e5e6
Jakub Krejcirik <jakub.krejcirik@plus4u.net>
2018-12-22 18:57:39 +0100

    idk

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -2,4 +2,20 @@ class BuildingsController < ApplicationController
   def index
     @buildings = Building.all
   end
+
+  def create
+    building = Building.new(building_params)
+    if building.save
+      redirect_to :adminBuildings
+    else
+      flash[:errors] = building.errors.full_messages
+      redirect_to :adminBuildings
+    end
+  end
+
+  # parameters
+  def building_params
+    params.require(:building).permit(:code, :title, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,16 @@ Rails.application.routes.draw do
   # root_path of app
   root "buildings#index"
 
+  # admin
+  get "adminBuildings" => "administration#buildings"
+  get "adminCourses" => "administration#courses"
+  get "adminLessons" => "administration#lessons"
+  get "adminTeachers" => "administration#teachers"
+  get "adminStudents" => "administration#students"
+  get "adminRooms" => "administration#rooms"
+  get "administration" => "administration#index"
+
+  resources :administration
   resources :buildings
   resources :courses
   resources :teachers
