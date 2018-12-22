40eb24183763e9ce196de15f60ea8af49ef7a1ff
Jakub Krejcirik <jakub.krejcirik@plus4u.net>
2018-12-22 20:19:05 +0100

    udpate CRUD for buildings!!!

---
+++ app/controllers/administration_controller.rb
@@ -0,0 +1,47 @@
+class AdministrationController < ApplicationController
+  def index
+
+  end
+
+  # building CRUD
+  def buildings # show buildings
+    @buildings_data = Building.all
+  end
+
+  def createBuilding
+    @buildings = Building.new
+  end
+
+  def editBuilding
+    @building = Building.find(params[:id])
+  end
+
+
+  # Course CRUD
+  def courses
+
+  end
+  # Lesson CRUD
+  def lessons
+
+  end
+
+  #Teachers CRUD
+  def teachers
+
+  end
+
+  # Students CRUD
+  def students
+
+  end
+
+  # Rooms CRUD
+  def rooms
+
+  end
+
+
+
+
+end
